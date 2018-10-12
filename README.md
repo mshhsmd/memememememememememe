935b794cb50da2f68af005c629eb25422da529c1
mshhsmd <44076388+mshhsmd@users.noreply.github.com>
2018-10-12 04:14:37 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# memememememememememe
-Ghadir
+# dragon-bot
---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'process.env.BOT_TOKEN'; 
+
+exports.PREFIX = '+';
+
+exports.GOOGLE_API_KEY = 'EzZnlVfE2kxqzwRjbhIWmxAk948L2Irq';
