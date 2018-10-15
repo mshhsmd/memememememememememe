2950bc48b528f528dfcc23535591ae7719f65b83
mshhsmd <44076388+mshhsmd@users.noreply.github.com>
2018-10-15 20:09:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4469,8 +4469,7 @@ client.on("message", message => {
       });
             client.login(process.env.BOT_TOKEN);
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
